ac4ff892df39c197c5b973c444a0d10c7ccc72cc
Joshua Prettyman <j.prettyman@icloud.com>
2026-02-19 11:58:13 +0100

    project basic boilerplate

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+"""Private Label Intelligence Engine.
+
+Identifies food categories where European retailers can launch
+health-positioned private label products into underserved nutritional gaps.
+"""
